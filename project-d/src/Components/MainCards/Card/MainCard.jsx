a3b16f1dfa300fcf1622e6e550165f382196927b
Aleg3000 <88538863+Aleg3000@users.noreply.github.com>
2023-01-13 11:53:32 +0530

    fix: animation on main page

--- project-d/src/Components/MainCards/Card/MainCard.jsx
+++ project-d/src/Components/MainCards/Card/MainCard.jsx
@@ -1,16 +1,16 @@
 import cl from './MainCard.module.css'
 
-const MainСard = ({title, index, slide: slideCard}) => {
+const MainСard = ({title, index, slide: slideCard, open}) => {
 
-    
     return (
         <div 
-            onMouseEnter={(e) => slideCard.call( null, 'open', e.currentTarget, index)} 
-            onMouseLeave={(e) => slideCard.call( null, 'close', e.currentTarget, index)} 
+            onMouseEnter={(e) => slideCard.call(null, 'open', e.currentTarget, index)} 
+            onMouseLeave={(e) => slideCard.call(null, 'close', e.currentTarget, index)}
+            onClick={open} 
             className={[cl.card, cl[`card${index + 1}`]].join(' ')}
-            >
-                <h2>{title}</h2>
-            </div>
+        >
+            <h2>{title}</h2>
+        </div>
     )
 }
 
--- project-d/src/Components/MainCards/MainCards.jsx
+++ project-d/src/Components/MainCards/MainCards.jsx
@@ -5,40 +5,28 @@ import cl from './MainCards.module.css'
 
 const MainCards = () => {
 
-    const container = createRef();
+    const container = createRef()
 
-    const colors = ['#CE5033', '#B15000', '#C22E20', '#7FA7A8', 'lime']
+    const colors = ['#CE5033', '#B15000', '#7FA7A8', '#C22E20', 'lime']
 
     const cards = [
         'PORSCHE DAKAR, Testing',
         'UNION HOOKAH, Packing',
         '3D LETTERING, Lettering',
         'AUDI QUATTRO DAYS, Branding',
-        'OUCE TYPEFACE, Fonts'
+        'OUCE TYPEFACE, Fonts',
     ]
 
     function slideCard(mode, card, i) {
-        console.log(...arguments);
-        // const title = card.querySelector('h2')
-        let translate = 27.25;
+        const title = card.firstChild
+        const translate = 27.25; /*  поправить расчеты */
+        
         if (mode === 'open') {
-            // card.addEventListener('click', (e) => {
-            //     const a = e.currentTarget.cloneNode(true);
-            //     a.style.position = 'absolute';
-            //     a.style.top = e.currentTarget.getBoundingClientRect().top + 'px';
-            //     a.style.left = e.currentTarget.getBoundingClientRect().left + 'px';
-            //     console.log(e.target.getBoundingClientRect())
-            //     document.body.appendChild(a);
-            //     gsap.to(a, {width: '100vw', height: '100vh', top: '0', left: '0', borderRadius: '0'})
-                
-            // })
-
             gsap.to('main', {backgroundColor: colors[i], duration: 1})
             // to fix
-            // gsap.to(title, {opacity: 1, duration: 1.5,})
+            gsap.to(title, {opacity: 1, duration: 1, delay: 0.3 })
             gsap.to(card, {width: '70rem', opacity: 1, ease: "expo.out", duration: 1})
             if (i === 2) return;
-            console.log(container)
             gsap.to(container.current, {
                 transform: `translateX(${i < 2 ? translate : - translate}rem)`,
                 ease: "expo.out",
@@ -48,17 +36,32 @@ const MainCards = () => {
             gsap.to(card, {width: '15.5rem', opacity: 1, ease: "expo.out", duration: 1})
             gsap.to('main', {backgroundColor: '#0F1010', duration: 1})
             // to fix
-            // gsap.to(title, {opacity: 0, duration: 0.1})
+            gsap.to(title, {opacity: 0, duration: 0.1, overwrite: true})
             if (i === 2) return;
             gsap.to(container.current, {transform: 'translateX(0)', ease: "expo.out", duration: 1})
         }
+    }
+
+    function openCard(e) {
+            const a = e.currentTarget.cloneNode(true)
+            a.style.position = 'absolute'
+            a.style.top = e.currentTarget.getBoundingClientRect().top + 'px'
+            a.style.left = e.currentTarget.getBoundingClientRect().left + 'px'
+            document.body.appendChild(a)
+            gsap.to(a, {width: '100vw', height: '100vh', top: '0', left: '0', borderRadius: '0'})
     }
 
     
 
     return (
         <div className={cl.mainContainer} ref={container}>
-            {cards.map((card, i) => <Card slide={slideCard} title={card} index={i} key={i+card[0]} />)}
+            {cards.map((card, i) => <Card 
+                                        open={openCard}
+                                        slide={slideCard} 
+                                        title={card} 
+                                        index={i} 
+                                        key={i+card[0]} 
+                                    />)}
         </div>
     )
 }
