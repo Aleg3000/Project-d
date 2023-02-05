4c09e9b996db3a4f5ffe5d298d4bb61bc61fff96
Aleg3000 <88538863+Aleg3000@users.noreply.github.com>
2023-02-05 15:34:23 +0530

    feat: add font page

--- src/Pages/FontPage/FontPage.jsx
+++ src/Pages/FontPage/FontPage.jsx
@@ -1,8 +1,39 @@
+import { useNavigate } from 'react-router-dom'
 import cl from './FontPage.module.css'
 
 const FontPage = () => {
+    const navigate = useNavigate()
     return (
-        <div>Font Page</div>
+        <div className={cl.container}>
+            <header className={cl.welcomeSection}>
+                <div onClick={() => navigate('/')} className={cl.titleBtn}>Project-d</div>
+                <h2>OUCE,<span> Type</span></h2>
+                <div className={cl.welcomeLogo}></div>
+             </header>
+
+             <main>
+                <section className={cl.firstSection}>
+                    <div className={cl.letter}></div>
+                    <div className={cl.letter}></div>
+                    <div className={cl.letter}></div>
+                    <div className={cl.letter}></div>
+                </section>
+
+                <section className={cl.secondSection}></section>
+
+                <section className={cl.thirdSection}>
+                    <div className={cl.mask}>
+                        {/* <div className={cl.transparent}></div>
+                        <div className={cl.transparent}></div>
+                        <div className={cl.transparent}></div> */}
+                    </div>
+                </section>
+             </main>
+
+             <footer className={cl.footer}>
+                <h2 onClick={() => navigate('/hookahCatalog')}>NEXT PROJECT</h2>
+            </footer>
+        </div>
     )
 }
 
