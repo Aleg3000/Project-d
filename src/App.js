54fbda014bd5f945dea8524bb947f8c124f98e4f
OLEG GANIN <88538863+Aleg3000@users.noreply.github.com>
2023-03-07 23:22:30 +0300

    feat: add orientation locker

--- src/App.js
+++ src/App.js
@@ -1,21 +1,49 @@
 import { useMatchMedia } from './hooks/useMatchMedia';
 import { RouterProvider } from 'react-router-dom';
 import { mobileRouter, desktopRouter } from './router/router';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Loader from './Components/Loader/Loader';
+import Locker from './Components/Locker/Locker';
+
 
 function App() {
 
   const { isMobile } = useMatchMedia();
 
   const [isLoading, setIsLoading] = useState(true)
+
+  const [isNormal, setIsNormal] = useState(true)
+
+  const checkOrientation = () => {
+    if (window.orientation === 90 || window.orientation === -90) {
+      setIsNormal(false)
+    } else {
+      setIsNormal(true)
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener("orientationchange", checkOrientation, false);
+    window.addEventListener("resize", checkOrientation, false);
+
+    checkOrientation()
+
+    return () => {
+      window.removeEventListener('orientationchange', checkOrientation)
+      window.removeEventListener('resize', checkOrientation)
+    }
+  }, [])
   
   return (
     isLoading
     ?
     <Loader setIsLoading={setIsLoading} />
     :
+    isNormal
+    ?
     <RouterProvider router={isMobile ? mobileRouter : desktopRouter} />
+    :
+    <Locker />
     )
 }
 
---
+++ src/Components/Locker/Locker.jsx
@@ -0,0 +1,9 @@
+import cl from './Locker.module.css'
+
+const Locker = () => {
+    return (<div className={cl.locker}>
+        <h2>Please rotate your device</h2>
+    </div>)
+  } 
+
+  export default Locker
